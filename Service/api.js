a520fbd827c432a3b6bdfccaabb3a9b4b01cd1e5
4m1g0 <dev.4m1g0@gmail.com>
2018-02-06 13:58:08 +0100

    Add sensor endpoints

--- Service/api.js
+++ Service/api.js
@@ -3,42 +3,71 @@ var status = require('http-status');
 var bodyparser = require('body-parser');
 
 module.exports = function(wagner) {
-  var api = express.Router();
+    var api = express.Router();
 
-  api.use(bodyparser.json());
+    api.use(bodyparser.json());
 
-  api.post('/history', wagner.invoke(function (RadonLog) {
-      return function (req, res) {
-          ///console.log(req.body);
-          var log = new RadonLog(req.body);
-          log.save(function (error, log) {
-              if (error) {
-                  return res.status(status.BAD_REQUEST).json({error: error.toString()});
-              }
+    api.post('/history', wagner.invoke(function (RadonLog) {
+        return function (req, res) {
+            var log = new RadonLog(req.body);
+            log.save(function (error, log) {
+                if (error) {
+                    return res.status(status.BAD_REQUEST).json({error: error.toString()});
+                }
 
-              return res.status(status.OK).json({log_id: log._id});
-          });
-      };
-  }));
+                return res.status(status.OK).json({log_id: log._id});
+            });
+        };
+    }));
 
+    api.get('/history', wagner.invoke(function (RadonLog) {
+        return function(req, res){
+            RadonLog.find({sensorId: req.query.sensorId, date : { $lt : "2018-01-25" ,
+                    $gt : "2018-01-01"}}, function(error, logs){
+                if (error) {
+                    return res.status(status.BAD_REQUEST).json({error: error.toString()});
+                }
 
-  api.get('/history', wagner.invoke(function (RadonLog) {
-      return function(req, res){
-          console.log(req.params);
-          RadonLog.find({sensorId: req.query.sensorId, date : { $lt : "2018-01-25" ,
-                  $gt : "2018-01-01"}}, function(error, logs){
-              if (error) {
-                  return res.status(status.BAD_REQUEST).json({error: error.toString()});
-              }
+                if (!logs) {
+                    return res.status(status.NOT_FOUND).json({error:'Not found'});
+                }
 
-              if (!logs) {
-                  return res.status(status.NOT_FOUND).json({error:'Not found'});
-              }
+                res.json({logs:logs});
+            });
+        };
+    }));
 
-              res.json({logs:logs});
-          });
-      };
-  }));
+    api.get('/sensor/:id', wagner.invoke(function (Sensor) {
+        return function(req, res){
+            Sensor.findOne({_id: req.params.id}, function(error, sensor){
+                if (error) {
+                    return res.status(status.BAD_REQUEST).json({error: error.toString()});
+                }
+
+                if (!sensor) {
+                    return res.status(status.NOT_FOUND).json({error:'Not found'});
+                }
+
+                res.json(sensor);
+            });
+        };
+    }));
+
+    api.get('/sensor', wagner.invoke(function (Sensor) {
+        return function(req, res){
+            Sensor.find({}, function(error, sensors){
+                if (error) {
+                    return res.status(status.BAD_REQUEST).json({error: error.toString()});
+                }
+
+                if (!sensors.length) {
+                    return res.status(status.NOT_FOUND).json({error:'Not found'});
+                }
+
+                res.json({sensors:sensors});
+            }).limit(100);
+        };
+    }));
 
   return api;
 };
