6582d70718b813c760986f6ef32d82cd4a9ef10f
4m1g0 <dev.4m1g0@gmail.com>
2018-07-27 12:55:26 +0200

    Add CORS headers0

--- Service/api.js
+++ Service/api.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var status = require('http-status');
 var bodyparser = require('body-parser');
-var dateFormat = require('dateformat');
 
 module.exports = function(wagner) {
     var api = express.Router();
@@ -12,13 +11,6 @@ module.exports = function(wagner) {
         return function (req, res) {
 	    console.log(req.body);
             var log = new RadonLog(req.body);
-
-            req.body.sensorId
-            dateFormat(Date.now(), "yyyy-mm-dd");
-
-            RadonLog.update({_id: req.params.id}, doc, function(err, raw) {
-                if (err) {
-
             log.save(function (error, log) {
                 if (error) {
                     return res.status(status.BAD_REQUEST).json({error: error.toString()});
--- Service/index.js
+++ Service/index.js
@@ -5,6 +5,22 @@ require('./models')(wagner);
 
 var app = express();
 
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');//'http://radon.4m1g0.com:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
+
 app.use('/radon/api/v1', require('./api')(wagner));
 
 app.listen(3000);
